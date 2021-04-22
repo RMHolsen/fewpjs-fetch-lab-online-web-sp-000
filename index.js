cc09990d4f377fb25cbcfddf5006bc4364b6e72e
rmholsen <jaguar.kitty@gmail.com>
2021-04-22 18:12:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(function(response) {
+        return response.json();
+      })
+    .then(function(json) {
+      renderBooks(json)
+      });
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
